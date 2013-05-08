a4d0d477a64fc712a8a16b81410d5dd6d7cbcce3
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2013-05-08 16:44:18 +0200

    added -D and -I

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "path.h"
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 class CppcheckExecutor : public ErrorLogger {
@@ -33,11 +34,73 @@ int main(int argc, char **argv)
 {
     std::cout << "ublinter" << std::endl;
 
+    Settings settings;
     std::map<std::string, std::size_t> files;
     for (int i = 1; i < argc; i++) {
-        std::string path = Path::fromNativeSeparators(argv[i]);
-        path = Path::removeQuotationMarks(path);
-        FileLister::recursiveAddFiles(files, Path::toNativeSeparators(path));
+        if (std::strncmp(argv[i], "-D", 2) == 0) {
+            std::string define;
+
+            // "-D define"
+            if (std::strcmp(argv[i], "-D") == 0) {
+                ++i;
+                if (i >= argc || argv[i][0] == '-') {
+                    std::cerr << "cppcheck: argument to '-D' is missing." << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                define = argv[i];
+            }
+            // "-Ddefine"
+            else {
+                define = 2 + argv[i];
+            }
+
+            // No "=", append a "=1"
+            if (define.find("=") == std::string::npos)
+                define += "=1";
+
+            // DEF= => empty define
+            else if (define.find("=") + 1U == define.size())
+                define.erase(define.size() - 1U);
+
+            if (!settings.userDefines.empty())
+                settings.userDefines += ";";
+            settings.userDefines += define;
+        }
+
+        // Include paths
+        else if (std::strncmp(argv[i], "-I", 2) == 0) {
+            std::string path;
+
+            // "-I path/"
+            if (std::strcmp(argv[i], "-I") == 0) {
+                ++i;
+                if (i >= argc || argv[i][0] == '-') {
+                    std::cerr << "cppcheck: argument to '-I' is missing." << std::endl;
+                    return false;
+                }
+                path = argv[i];
+            }
+
+            // "-Ipath/"
+            else {
+                path = 2 + argv[i];
+            }
+            path = Path::fromNativeSeparators(path);
+            path = Path::removeQuotationMarks(path);
+
+            // If path doesn't end with / or \, add it
+            if (path[path.length()-1] != '/')
+                path += '/';
+
+            settings._includePaths.push_back(path);
+        }
+
+        else {
+            std::string path = Path::fromNativeSeparators(argv[i]);
+            path = Path::removeQuotationMarks(path);
+            FileLister::recursiveAddFiles(files, Path::toNativeSeparators(path));
+        }
     }
 
     CppcheckExecutor cppcheckExecutor;

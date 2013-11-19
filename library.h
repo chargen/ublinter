8e58ab3bfbb1a1292d40a1996adcd6b17062a5c2
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2013-11-19 19:27:38 +0100

    astyle formatting

--- library.h
+++ library.h
@@ -2,7 +2,13 @@
 #include <string>
 
 struct Library {
-  bool isnoreturn(const std::string &) const { return false; }
-  bool isnotnoreturn(const std::string &) const { return false; };
-  bool reportErrors(const std::string &) const { return true; }
+    bool isnoreturn(const std::string &) const {
+        return false;
+    }
+    bool isnotnoreturn(const std::string &) const {
+        return false;
+    };
+    bool reportErrors(const std::string &) const {
+        return true;
+    }
 };

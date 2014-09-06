e83516d4b74e8e75bcb8805da78e33423a7b5484
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2014-09-06 19:11:18 +0200

    added integer overflow checker

--- ublinter.py
+++ ublinter.py
@@ -3,6 +3,7 @@
 import cppcheckdata
 import sys
 
+# division by zero
 def checkdivision(data):
   for token in data.tokenlist:
     # not division
@@ -27,11 +28,30 @@ def checkdivision(data):
     # Denominator is not known to be non-zero..
     print('[' + token.file + ':' + str(token.linenr) + '] Division by zero')
 
+def isIntMax(v):
+    if v == 0x7fff:
+        return 16
+    if v == 0x7fffffff:
+        return 32
+    if v == 0x7fffffffffffffff:
+        return 64
+    return None
+
+def checkintegeroverflow(data):
+    for token in data.tokenlist:
+        # INT_MAX - svar
+        if token.str=='-' and token.astOperand2 and token.astOperand1.isInt and token.astOperand2.isName:
+            bits = isIntMax(int(token.astOperand1.str))
+            if bits:
+                print('[' + token.file + ':' + str(token.linenr) + '] Signed integer overflow if ' + token.astOperand2.str + ' is negative (' + str(bits) + ' bit int)')
+
 def checkuninit(data):
-  return
+    # TODO
+    return
 
 for arg in sys.argv[1:]:
   print('Checking ' + arg + '...')
   data = cppcheckdata.parsedump(arg)
-  checkdivision(data)
-  checkuninit(data)
+  checkdivision(data)         # division by zero
+  checkintegeroverflow(data)  # integer overflow
+  checkuninit(data)           # uninitialized variable
